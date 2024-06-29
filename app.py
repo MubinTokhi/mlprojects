e10039f2e17183c11c826810ac0e3963a8b093d5
Mubin Ahmad Tokhi <yaseentokhi44@gmail.com>
2024-06-29 01:16:47 -0400

    data ingestion complete

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+
 from src.logger import logging
 import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pickle
+ 
 from src.exception import CustomException
 
+from src.components.data_ingestion import DataIngestion, DataIngestionConfig
 
-logging.info('Hellow worlds')
-
-try:
-    a = 1 + "c"
-except Exception as e:
-    logging.info(CustomException(e,sys))
-    raise CustomException(e, sys) from e 
+obj = DataIngestion()
+obj.initiate_data_ingestion() 
